59aa69712d631d399ffffc97439fa7924be8de18
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-01-06 13:44:21 +0000

    modelling new Articles collection

--- models/models.js
+++ models/models.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-// module.exports.gallery = mongoose.model('Gallery', Schema({
-// 	tag: String,
-// 	set_id: String,
-// 	label: String,
-// 	index: Number
-// }));
+module.exports.article = mongoose.model('Article', Schema({
+	headline: String,
+	headline_images: [String],
+    textbody: String,
+    textbody_media: [String]
+}));
