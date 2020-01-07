65f24376a74c66172219966f998fd4d23cfdb3de
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-01-07 01:53:50 +0000

    developed article posting process, with cloud management for images enabled

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var cloud = require('cloudinary');
+var Article = require('../models/models').article;
 // var nodemailer = require('nodemailer');
-// var models = require('../models/models');
 
 router.get('/', (req, res) => {
 	res.render('index', { title: null, pagename: "home" })
@@ -32,8 +33,36 @@ router.get('/admin', (req, res) => {
 });
 
 router.post('/news/article/new', (req, res) => {
-	console.log(req.body);
-	res.send("done!!!");
+	var article = new Article({
+		headline: req.body.headline,
+		textbody: req.body.textbody
+	});
+
+	article.save((err, doc) => {
+		["headline_images", "textbody_media", "headline_images[]", "textbody_media[]"].forEach(field => {
+			if (req.body[field]) {
+				doc[field] = [];
+				req.body[field] = typeof req.body[field] === "string" ? [req.body[field]] : req.body[field];
+				req.body[field].forEach((image, i) => {
+					var f = field.replace("[]", "");
+					cloud.v2.uploader.upload(image, { public_id: ("article/"+ doc.id +"/"+ f + i+1) }, (err, result) => {
+						if (err) console.log(err);
+						doc[f].push(result.url);
+						doc.save();
+					});
+				})
+			}
+		});
+		res.send("DONE")
+	});
+});
+
+router.post('/news/article/delete/:id', (req, res) => {
+	Article.findByIdAndDelete(req.params.id, function(err, doc) {
+		if (err || !doc) return res.send(err || "Article not found");
+		cloud.v2.api.delete_resources_by_prefix('article/'+doc.id, (err, result) => { console.log(result, err) });
+		res.redirect(req.get("referrer"))
+	})
 });
 
 // router.post('/send/message', (req, res) => {
