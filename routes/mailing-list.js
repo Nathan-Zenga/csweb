4d496f45f733ce92e1382e6ecf5851391fe5a827
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-04-03 00:15:36 +0100

    using OAUTH2 for secure email transporting

--- routes/mailing-list.js
+++ routes/mailing-list.js
@@ -1,56 +1,61 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
+var { OAuth2 } = require("googleapis").google.auth;
+var { OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_REFRESH_TOKEN } = process.env;
 var { MailingList } = require('../models/models');
 
 router.get('/sign-up', (req, res) => {
-	res.render('mailing-list', { title: "Sign Up", pagename: "sign-up" })
+    res.render('mailing-list', { title: "Sign Up", pagename: "sign-up" })
 });
 
 router.post('/new', (req, res) => {
-	var newMember = new MailingList({
-		firstname: req.body.firstname,
-		lastname: req.body.lastname,
-		email: req.body.email,
-		size_top: req.body.size_top,
-		size_bottom: req.body.size_bottom
-	});
+    var { firstname, lastname, email, size_top, size_bottom } = req.body;
+    var newMember = new MailingList({ firstname, lastname, email, size_top, size_bottom });
 
-	MailingList.findOne({email: req.body.email}, (err, member) => {
-		!member ? newMember.save(err => { res.send("YOU ARE NOW REGISTERED") }) : res.send("ALREADY REGISTERED")
-	})
+    MailingList.findOne({ email }, (err, member) => {
+        if (err || member) return res.send(err || "ALREADY REGISTERED");
+        newMember.save(err => res.send(err || "YOU ARE NOW REGISTERED"))
+    })
 });
 
 router.post('/send/email', (req, res) => {
-	MailingList.find((err, members) => {
-		var transporter = nodemailer.createTransport({
-			service: 'gmail',
-			port: 465,
-			secure: true,
-			auth: {
-				user: NODEMAILER_AUTH_USER,
-				pass: NODEMAILER_AUTH_PASS
-			},
-			tls: {
-				rejectUnauthorized: true
-			}
-		});
+    MailingList.find((err, members) => {
+        members.forEach((member, i) => {
+            var oauth2Client = new OAuth2( OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, "https://developers.google.com/oauthplayground" );
+            oauth2Client.setCredentials({ refresh_token: OAUTH_REFRESH_TOKEN });
+            var accessToken = oauth2Client.getAccessToken();
 
-		var emails = members.map(member => member.email);
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                // port: 465,
+                // secure: true,
+                auth: {
+                    type: "OAuth2",
+                    user: "info@thecs.co",
+                    clientId: OAUTH_CLIENT_ID,
+                    clientSecret: OAUTH_CLIENT_SECRET,
+                    refreshToken: OAUTH_REFRESH_TOKEN,
+                    accessToken
+                },
+                tls: {
+                    rejectUnauthorized: true
+                }
+            });
 
-		var mailOptions = {
-			from: { name: req.body.name, address: req.body.email },
-			to: emails,
-			subject: req.body.subject,
-			html: `From ${req.body.name} (${req.body.email}):\n\n${req.body.message}`
-		};
-
-		transporter.sendMail(mailOptions, err => {
-			if (err) return console.log(err), res.send("COULD NOT SEND MESSAGE");
-			transporter.close();
-			res.send('MESSAGE SENT');
-		});
-	})
+            transporter.sendMail({
+                from: "info@thecs.co",
+                to: member.email,
+                subject: req.body.subject,
+                html: req.body.message.replace(/\=FN\=/g, member.firstname).replace(/\=LN\=/g, member.lastname)
+            }, err => {
+                if (err) return console.log(err), res.send("Could not send message. Error occurred.");
+                console.log("The message was sent!");
+                transporter.close();
+                if (i === members.length-1) res.send("DONE")
+            });
+        })
+    })
 });
 
 module.exports = router;
