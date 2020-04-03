b25cf44c14bf78cf1d6458a1d9c17271cc27e7b5
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-04-03 22:58:20 +0100

    set-up for management and display of homepage banner and images

--- models/models.js
+++ models/models.js
@@ -2,43 +2,53 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports.Article = mongoose.model('Article', Schema({
-	headline: String,
-	headline_images: [String],
-	headline_image_thumb: String,
-	textbody: String,
-	textbody_media: [String]
+    headline: String,
+    headline_images: [String],
+    headline_image_thumb: String,
+    textbody: String,
+    textbody_media: [String]
 }, { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } }));
 
 module.exports.Project = mongoose.model('Project', Schema({
-	title: String,
-	artist: String,
-	year: Number,
-	artwork: String,
-	links: [String],
-	all_platforms: { type: Boolean, default: false }
+    title: String,
+    artist: String,
+    year: Number,
+    artwork: String,
+    links: [String],
+    all_platforms: { type: Boolean, default: false }
 }));
 
 module.exports.Artist = mongoose.model('Artist', Schema({
-	name: String,
-	bio: String,
-	socials: Schema.Types.Mixed,
-	profile_image: String
+    name: String,
+    bio: String,
+    socials: Schema.Types.Mixed,
+    profile_image: String
 }));
 
 module.exports.MailingList = mongoose.model('MailingList', Schema({
-	fistname: String,
-	lastname: String,
-	email: String,
-	size_top: String,
-	size_bottom: String
+    fistname: String,
+    lastname: String,
+    email: String,
+    size_top: String,
+    size_bottom: String
 }));
 
 module.exports.Location = mongoose.model('Location', Schema({
-	name: String,
-	street_address: String,
-	city: String,
-	country: String,
-	postcode: String,
-	latitude: Number,
-	longitude: Number
+    name: String,
+    street_address: String,
+    city: String,
+    country: String,
+    postcode: String,
+    latitude: Number,
+    longitude: Number
+}));
+
+module.exports.Homepage_banner = mongoose.model('Homepage_banner', Schema({
+    text: String,
+    banner_media: [String]
+}));
+
+module.exports.Homepage_image = mongoose.model('Homepage_image', Schema({
+    p_id: String,
+    image: String
 }));
--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,91 @@
 var express = require('express');
 var router = express.Router();
-var { Article, Project, Artist, Location } = require('../models/models');
+var cloud = require('cloudinary');
+var { Article, Project, Artist, Location, Homepage_banner, Homepage_image } = require('../models/models');
+var Collections = cb => {
+    Article.find().sort({ created_at: -1 }).exec((err, articles) => {
+        Artist.find((err, artists) => {
+            Project.find().sort({ year: -1 }).exec((err, projects) => {
+                Location.find((err, locations) => {
+                    Homepage_banner.find((err, banners) => {
+                        Homepage_image.find((err, homepage_images) => {
+                            cb({ articles, artists, projects, locations, banners, homepage_images });
+                        })
+                    })
+                })
+            })
+        })
+    })
+};
 
 router.get('/', (req, res) => {
-	res.render('index', { title: null, pagename: "home" })
+    Homepage_banner.findOne((err, banner) => {
+        Homepage_image.find((err, images) => {
+            res.render('index', { title: null, pagename: "home", banner, images })
+        })
+    })
 });
 
 router.get('/admin', (req, res) => {
-	Article.find().sort({ created_at: -1 }).exec((err, articles) => {
-		Artist.find((err, artists) => {
-			Project.find().sort({ year: -1 }).exec((err, projects) => {
-				Location.find((err, locations) => {
-					res.render('admin', { title: "Admin", pagename: "admin", db: {articles, artists, projects, locations} })
-				})
-			})
-		})
-	})
+    Collections(db => res.render('admin', { title: "Admin", pagename: "admin", db }))
 });
 
 router.post('/search', (req, res) => {
-	Article.find().sort({ created_at: -1 }).exec((err, articles) => {
-		Artist.find((err, artists) => {
-			Project.find().sort({ year: -1 }).exec((err, projects) => {
-				Location.find((err, locations) => {
-					res.send([...articles, ...artists, ...projects, ...locations]);
-				})
-			})
-		})
-	})
+    Collections(db => {
+        var { articles, artists, projects, locations, banners, homepage_images } = db;
+        res.send([...articles, ...artists, ...projects, ...locations, ...banners, ...homepage_images]);
+    })
+});
+
+router.post('/homepage/banner', (req, res) => {
+    var saveMedia = (doc, cb) => {
+        var msg = "", { banner_media } = req.body;
+        if (banner_media) {
+            msg = " - saving images / videos";
+            banner_media.split(",").forEach((mediaStr, i) => {
+                doc.banner_media = [];
+                var public_id = "homepage/banner/image" + (i+1);
+                cloud.v2.uploader.upload(mediaStr, { public_id, resource_type: "auto" }, (err, result) => {
+                    if (err) res.send(err);
+                    doc.banner_media.push(result.secure_url);
+                    doc.save();
+                });
+            });
+        }
+        if (cb) cb(msg);
+    };
+
+    Homepage_banner.find((err, banners) => {
+        var banner = !banners.length ? new Homepage_banner() : banners[0];
+        banner.text = req.body.text;
+        banner.save((err, saved) => {
+            if (err) return res.send(err);
+            cloud.v2.api.delete_resources_by_prefix("homepage/banner/", err => {
+                if (err) return res.send(err);
+                saveMedia(saved, msg => res.send("BANNER " + (!banners.length ? "SAVED" : "UPDATED") + msg))
+            });
+        });
+    })
+});
+
+router.post('/homepage/image/save', (req, res) => {
+    cloud.v2.uploader.upload(req.body.image, { public_id: `homepage/images/${req.body.filename}` }, (err, result) => {
+        if (err) return res.send(err);
+        var newImage = new Homepage_image({ image: result.secure_url });
+        newImage.save(err => {
+            if (err) return res.send(err);
+            saved.p_id = result.public_id;
+            saved.save(err => res.send(err || "IMAGE SAVED"));
+        })
+    })
+});
+
+router.post('/homepage/image/delete', (req, res) => {
+    var { p_id } = req.body;
+    cloud.v2.api.delete_resources([ p_id ], err => {
+        if (err) return res.send(err);
+        Homepage_image.deleteOne({ p_id }, err => res.send(err || "IMAGE REMOVED"))
+    })
 });
 
 module.exports = router;
