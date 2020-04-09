c9b6e0caf370caf8b65dabddda0156b4cb73c217
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-04-09 23:46:13 +0100

    changes for enabling both individual and mass emails

--- routes/mailing-list.js
+++ routes/mailing-list.js
@@ -34,16 +34,16 @@ router.post('/update', (req, res) => {
 });
 
 router.post('/send/mail', (req, res) => {
+    var { email, subject, message } = req.body;
     MailingList.find((err, members) => {
         if (err || !members.length) return res.send(err || "NO MEMBERS IN THE MAILING LIST TO SEND THE EMAIL TO");
 
         var oauth2Client = new OAuth2( OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, "https://developers.google.com/oauthplayground" );
         oauth2Client.setCredentials({ refresh_token: OAUTH_REFRESH_TOKEN });
         var accessToken = oauth2Client.getAccessToken();
+        members = email ? members.filter(m => m.email === email) : members;
 
         members.forEach((member, i) => {
-            var { firstname, lastname, email, size_top, size_bottom } = member;
-            var refs = {"=FN=": firstname, "=LN=": lastname, "=ST=": size_top, "=SB=": size_bottom};
             var transporter = nodemailer.createTransport({
                 service: 'gmail',
                 // port: 465,
@@ -61,11 +61,11 @@ router.post('/send/mail', (req, res) => {
                 }
             });
 
-            res.render('templates/mail', { message: req.body.message, refs }, (err, html) => {
+            res.render('templates/mail', { message }, (err, html) => {
                 transporter.sendMail({
                     from: "CS <info@thecs.co>",
-                    to: email,
-                    subject: req.body.subject,
+                    to: member.email,
+                    subject,
                     html,
                     attachments: [{ filename: 'cs-icon.png', path: 'public/img/cs-icon.png', cid: 'logo' }]
                 }, err => {
@@ -73,8 +73,8 @@ router.post('/send/mail', (req, res) => {
                     console.log("The message was sent!");
                     transporter.close();
                     if (i === members.length-1) res.send("MESSAGE SENT")
-                });
-            });
+                })
+            })
         })
     })
 });
