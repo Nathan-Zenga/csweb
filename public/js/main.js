6681b635b170e4f1c4c541c2373229624c2a8206
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-04-12 23:15:28 +0100

    resolving style and formatting anomalies in news article pages
    optimising logo animation during scroll event

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,17 @@
 $(function() {
-	var height = $("#cs-logo").css("height");
+    var state = window.pageYOffset > $("main").offset().top+20;
+    $(window).on("load scroll", function() {
+        if (state != window.pageYOffset > $("main").offset().top+20) {
+            state = window.pageYOffset > $("main").offset().top+20;
+            $("#cs-logo").fadeOut(200, function() {
+                $(this).toggleClass("fixed", state).parent("a").attr("href", "/").removeAttr(state ? "href" : "").end().fadeIn(200);
+            });
+        }
+    });
 
-	$(window).on("load scroll", function() {
-		var state = window.pageYOffset > $("main").offset().top+20;
-		$("#cs-logo").toggleClass("fixed", state).parent("a").attr("href", "/").removeAttr(state ? "href" : "");
-	});
-
-	$("#nav-button").click(function() {
-		$(this).toggleClass("nav-open");
-		var navOpen = $(this).hasClass("nav-open");
-		$("nav").stop().slideToggle(function() { if(!navOpen) $(this).css("display", "") });
-	});
+    $("#nav-button").click(function() {
+        $(this).toggleClass("nav-open");
+        var navOpen = $(this).hasClass("nav-open");
+        $("nav").stop().slideToggle(function() { if(!navOpen) $(this).css("display", "") });
+    });
 });
