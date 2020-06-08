6307e3f32d34a0589191fa7d714d9790f3ea7984
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-06-08 21:40:22 +0100

    only activating OAuth2 client + access tokens in production; correcting error passage during async loop

--- routes/mailing-list.js
+++ routes/mailing-list.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
-const async = require('async');
+const { each } = require('async');
 const { OAuth2 } = require("googleapis").google.auth;
 const { OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, OAUTH_REFRESH_TOKEN, NODE_ENV } = process.env;
 const { MailingList } = require('../models/models');
@@ -24,28 +24,29 @@ router.post('/update', (req, res) => {
     var { member_id, firstname, lastname, email, size_top, size_bottom, extra_info } = req.body;
     MailingList.findById(member_id, (err, member) => {
         if (err || !member) return res.send(err || "MEMBER NOT FOUND");
-        member.firstname = firstname || member.firstname;
-        member.lastname = lastname || member.lastname;
-        member.email = email || member.email;
-        member.size_top = size_top || member.size_top;
-        member.size_bottom = size_bottom || member.size_bottom;
-        member.extra_info = extra_info || member.extra_info;
+        if (firstname)   member.firstname = firstname;
+        if (lastname)    member.lastname = lastname;
+        if (email)       member.email = email;
+        if (size_top)    member.size_top = size_top;
+        if (size_bottom) member.size_bottom = size_bottom;
+        if (extra_info)  member.extra_info = extra_info;
         member.save(err => res.send(err || "MEMBER UPDATED"));
     })
 });
 
 router.post('/send/mail', (req, res) => {
-    var { email, subject, message } = req.body;
+    var { email, subject, message } = req.body, oauth2Client, accessToken, sentCount = 0;
     MailingList.find(email ? { email } : {}, (err, members) => {
         if (err || !members.length) return res.send(err || "NO MEMBERS IN THE MAILING LIST TO SEND THE EMAIL TO");
 
-        var oauth2Client = new OAuth2( OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, "https://developers.google.com/oauthplayground" );
-        oauth2Client.setCredentials({ refresh_token: OAUTH_REFRESH_TOKEN });
-        var accessToken = oauth2Client.getAccessToken();
-        var sentCount = 0, errCount = 0;
+        if (NODE_ENV === "production") {
+            oauth2Client = new OAuth2( OAUTH_CLIENT_ID, OAUTH_CLIENT_SECRET, "https://developers.google.com/oauthplayground" );
+            oauth2Client.setCredentials({ refresh_token: OAUTH_REFRESH_TOKEN });
+            accessToken = oauth2Client.getAccessToken();
+        }
 
         nodemailer.createTestAccount((err, acc) => {
-            async.each(members, (member, cb) => {
+            each(members, (member, cb) => {
                 var transporter;
                 if (NODE_ENV === "production") {
                     transporter = nodemailer.createTransport({
@@ -78,14 +79,15 @@ router.post('/send/mail', (req, res) => {
                         attachments: [{ filename: 'cs-icon.png', path: 'public/img/cs-icon.png', cid: 'logo' }]
                     };
                     transporter.sendMail(msg, err => {
-                        err ? errCount += 1 : sentCount += 1;
-                        console.log(err || "The message was sent!");
-                        cb(err);
+                        if (err) return console.error(err), cb(err);
+                        sentCount += 1;
+                        console.log("The message was sent!");
+                        cb();
                     })
                 })
             }, err => {
                 if (err) console.log(err);
-                res.send(`MESSAGE SENT TO ${sentCount} MEMBERS${errCount ? " ("+ errCount +"ERRORS)" : ""}`);
+                res.send(`Message sent to ${sentCount}/${members.length} members${err ? " Error occurred" : ""}`);
             });
         });
     })
