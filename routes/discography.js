f4a0a0dae1202211b7bcddc18aec8f54f168d8c3
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-07-06 10:44:58 +0100

    follow-up changes concerning links entry + file upload process for discography project + artists

--- routes/discography.js
+++ routes/discography.js
@@ -43,30 +43,33 @@ router.post('/project/delete', (req, res) => {
 });
 
 router.post('/project/edit', (req, res) => {
-    var links = req.body.links_edit instanceof Array ? req.body.links_edit : [req.body.links_edit].filter(e => e);
+    const { link_name, link_url, project_id, title, artist, year, artwork_url, all_platforms, artwork_file } = req.body;
+    const link_names = (link_name instanceof Array ? link_name : [link_name]).filter(e => e);
+    const link_urls = (link_url instanceof Array ? link_url : [link_url]).filter(e => e);
+    if (link_names.length !== link_urls.length) return res.send("Number of specified link names + urls don't match");
 
-    Project.findById(req.body.project_id, (err, project) => {
-        if (req.body.title_edit) project.title = project.title;
-        if (req.body.artist_edit) project.artist = project.artist;
-        if (req.body.year_edit) project.year = project.year;
-        if (req.body.artwork_url_edit) project.artwork = project.artwork;
-        if (links && links.length) project.links = links;
-        project.all_platforms = !!req.body.all_platforms_change;
+    Project.findById(project_id, (err, project) => {
+        if (title) project.title = project.title;
+        if (artist) project.artist = project.artist;
+        if (year) project.year = project.year;
+        if (artwork_url) project.artwork = project.artwork;
+        if (link_names.length && link_urls.length) {
+            project.links = [];
+            link_names.forEach((name, i) => { project.links.push({ name: link_names[i], url: link_urls[i] }) });
+        }
+        project.all_platforms = !!all_platforms;
 
         project.save((err, saved) => {
-            var message_update = "Done";
-            if (req.body.artwork_file_change) {
-                cloud.v2.api.delete_resources_by_prefix("discography/" + saved.id, (err1, result) => {
-                    message_update += err ? ": error occurred, could not save artwork" : ": artwork saved";
-                    var public_id = "discography/"+ saved.id +"/"+ saved.title.replace(/ /g, "-");
-                    cloud.v2.uploader.upload(req.body.artwork_file_change, { public_id }, (err2, result) => {
-                        if (err1 || err2) return res.status(500).send((err1 || err2).message);
-                        saved.artwork = result.secure_url;
-                        saved.save();
-                    });
+            if (!artwork_file) return res.send("Done");
+            cloud.v2.api.delete_resources_by_prefix("discography/" + saved.id, (err1, result) => {
+                message_update += err ? ": error occurred, could not save artwork" : ": artwork saved";
+                var public_id = "discography/"+ saved.id +"/"+ saved.title.replace(/ /g, "-");
+                cloud.v2.uploader.upload(artwork_file, { public_id }, (err2, result) => {
+                    if (err1 || err2) return res.status(500).send((err1 || err2).message);
+                    saved.artwork = result.secure_url;
+                    saved.save(() => res.send("Done - artwork saved"));
                 });
-            }
-            res.send(message_update);
+            });
         });
     })
 });
