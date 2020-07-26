acf5dfc6bb90d1029249d2b86332d0454b3049e9
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-07-26 19:58:26 +0100

    creating a PaymentIntent object on form submit event, containing additional opts from passed form fields

--- routes/shop.js
+++ routes/shop.js
@@ -1,17 +1,16 @@
 const router = require('express').Router();
-const stripe = require("stripe")(process.env.STRIPE_SK);
+const stripe = require('stripe')(process.env.STRIPE_SK);
 
 router.get("/", (req, res) => {
     res.render('shop', { title: "Shop", pagename: "shop" })
 });
 
 router.get("/checkout", (req, res) => {
-    const { cart } = req.session;
-    res.render('checkout', { title: "Checkout", pagename: "checkout", pk: process.env.STRIPE_PK, cart })
+    res.render('checkout', { title: "Checkout", pagename: "checkout", pk: process.env.STRIPE_PK })
 });
 
 router.get("/cart", (req, res) => {
-    res.end()
+    res.render('checkout', { title: "Checkout", pagename: "checkout", cart: req.session.cart || [] })
 });
 
 router.post("/cart/add", (req, res) => {
@@ -20,12 +19,12 @@ router.post("/cart/add", (req, res) => {
     const cartItemIndex = req.session.cart.findIndex(item => item.product_id === product_id);
 
     if (cartItemIndex >= 0) {
-        req.session.cart[cartItemIndex].quantity += 1;
+        req.session.cart[cartItemIndex].qty += 1;
     } else {
-        req.session.cart.unshift({ product_id, name, price, category, quantity: 1 });
+        req.session.cart.unshift({ product_id, name, price, category, qty: 1 });
     }
 
-    res.send(req.session.cart.length.toString());
+    res.send(`${req.session.cart.length}`);
 });
 
 router.post("/cart/remove", (req, res) => {
@@ -33,10 +32,17 @@ router.post("/cart/remove", (req, res) => {
 });
 
 router.post("/checkout/create-payment-intent", async (req, res) => {
-    const { items } = req.body;
-    // Create a PaymentIntent with the order amount and currency
-    const amount = items.map(item => item.price).reduce((sum, val) => sum + val);
-    const paymentIntent = await stripe.paymentIntents.create({ amount, currency: "gbp" });
+    const { firstname, lastname, email, cart, address, city, postcode } = Object.assign(req.body, req.session);
+    const paymentIntent = await stripe.paymentIntents.create({ // Create a PaymentIntent with the order details
+        receipt_email: email,
+        description: cart.map(p => `${p.name} (£${(p.price / 100).toFixed(2)} X ${p.qty})`).join("\r\n"),
+        amount: cart.map(p => p.price).reduce((sum, val) => sum + val),
+        currency: "gbp",
+        shipping: {
+            name: firstname + " " + lastname,
+            address: { line1: address, city, postal_code: postcode }
+        }
+    });
 
     res.send({ clientSecret: paymentIntent.client_secret });
 });
