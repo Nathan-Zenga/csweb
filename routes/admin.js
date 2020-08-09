465ffe9a25e3f9f2e64e4d61f8e15c1eb7060d5a
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-08-09 16:48:35 +0100

    removing many documents in admin collection in case of duplicate temp docs stored

--- routes/admin.js
+++ routes/admin.js
@@ -30,22 +30,24 @@ router.post('/login', (req, res) => {
         if (err) return res.status(500).send(err.message || err);
         if (!user) return res.status(400).send(info.message);
         if (user === "to_activate") {
-            crypto.randomBytes(20, (err, buf) => {
-                let password = buf.toString("hex");
-                let tokenExpiryDate = new Date(Date.now() + (1000 * 60 * 60 * 2));
-                new Admin({ password, tokenExpiryDate }).save((err, doc) => {
-                    new MailingListMailTransporter({ req, res }, { email }).sendMail({
-                        subject: "Admin Account Activation",
-                        message: "You're recieving this email because an admin account needs setting up. " +
-                            "Please click the link below to activate the account, as this will only be " +
-                            "<u>available for the next 2 hours</u> from the time of this email received:\n\n" +
-                            `${res.locals.location_origin}/admin/activate/${doc.password}\n\n`
-                    }, err => {
-                        if (err) return res.status(500).send(err.message || err);
-                        res.status(400).send(info.message);
-                    });
+            Admin.deleteMany({ email: "temp" }, err => {
+                crypto.randomBytes(20, (err, buf) => {
+                    let password = buf.toString("hex");
+                    let tokenExpiryDate = new Date(Date.now() + (1000 * 60 * 60 * 2));
+                    new Admin({ email: "temp", password, tokenExpiryDate }).save((err, doc) => {
+                        new MailingListMailTransporter({ req, res }, { email }).sendMail({
+                            subject: "Admin Account Activation",
+                            message: "You're recieving this email because an admin account needs setting up. " +
+                                "Please click the link below to activate the account, as this will only be " +
+                                "<u>available for the next 2 hours</u> from the time of this email received:\n\n" +
+                                `${res.locals.location_origin}/admin/activate/${doc.password}\n\n`
+                        }, err => {
+                            if (err) return res.status(500).send(err.message || err);
+                            res.status(400).send(info.message);
+                        });
+                    })
                 })
-            })
+            });
         } else {
             req.logIn(user, err => {
                 res.status(err ? 500 : 200).send(err ? err.message || err : "/admin")
@@ -61,7 +63,7 @@ router.post("/activate/:token", async (req, res) => {
         if (!user) return res.status(400).send(info.message);
         req.logIn(user, err => {
             if (err) return res.status(500).send(err.message || err);
-            Admin.deleteOne({ password: req.params.token }, err => {
+            Admin.deleteMany({ email: "temp" }, err => {
                 res.status(err ? 500 : 200).send(err ? err.message : "/admin")
             });
         });
