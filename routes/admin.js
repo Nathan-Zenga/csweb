0369fe08351ac39fa4e1da1d4362ce3475ebf60f
Nathan-Zenga <zengaman@uni.coventry.ac.uk>
2020-08-09 05:11:58 +0100

    resolved reference error issues + set status code after email sent

--- routes/admin.js
+++ routes/admin.js
@@ -15,7 +15,7 @@ router.get('/login', (req, res) => {
     res.render('admin-login', { title: "Admin Login", pagename: "admin" })
 });
 
-router.get('/activate/:token', (req, res) => {
+router.get('/activate/:token', (req, res, next) => {
     Admin.findOne({ password: req.params.token, tokenExpiryDate: { $gte: Date.now() } }, (err, found) => {
         if (!found) return next();
         res.render('admin-activate', { title: "Admin Register", pagename: "admin", token: found.password })
@@ -30,20 +30,22 @@ router.post('/login', (req, res) => {
         if (err) return res.status(500).send(err.message || err);
         if (!user) return res.status(400).send(info.message);
         if (user === "to_activate") {
-            new MailingListMailTransporter({ req, res }, { email }).sendMail({
-                subject: "Admin Account Activation",
-                message: "You're recieving this email because an admin account needs setting up. " +
-                    "Please click the link below to activate the account, as this will only be available " +
-                    "for the next 2 hours from the time of this email received:\n\n" +
-                    `- ${res.locals.location_origin}/admin/activate/${doc.password}\n\n`
-            }, err => {
-                if (err) return res.status(500).send(err.message || err);
-                crypto.randomBytes(20, (err, buf) => {
-                    let password = buf.toString("hex");
-                    let tokenExpiryDate = new Date(Date.now() + (1000 * 60 * 60 * 2));
-                    new Admin({ password, tokenExpiryDate }).save((err, doc) => { res.send(info.message) })
+            crypto.randomBytes(20, (err, buf) => {
+                let password = buf.toString("hex");
+                let tokenExpiryDate = new Date(Date.now() + (1000 * 60 * 60 * 2));
+                new Admin({ password, tokenExpiryDate }).save((err, doc) => {
+                    new MailingListMailTransporter({ req, res }, { email }).sendMail({
+                        subject: "Admin Account Activation",
+                        message: "You're recieving this email because an admin account needs setting up. " +
+                            "Please click the link below to activate the account, as this will only be " +
+                            "<u>available for the next 2 hours</u> from the time of this email received:\n\n" +
+                            `${res.locals.location_origin}/admin/activate/${doc.password}\n\n`
+                    }, err => {
+                        if (err) return res.status(500).send(err.message || err);
+                        res.status(400).send(info.message);
+                    });
                 })
-            });
+            })
         } else {
             req.logIn(user, err => {
                 res.status(err ? 500 : 200).send(err ? err.message || err : "/admin")
